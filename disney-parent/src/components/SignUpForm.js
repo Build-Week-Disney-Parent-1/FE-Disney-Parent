a9b52977e186c3d683548966db123b159a8e54d9
Ewa Czech <ewa.czech@hotmail.com>
2020-02-27 14:40:52 +0100

    add yup validation schema and error notifications

--- disney-parent/src/components/SignUpForm.js
+++ disney-parent/src/components/SignUpForm.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
 
-function SignUpForm() {
+function SignUpForm({ errors, touched }) {
 	return (
 		<Form>
 			<h2>Sign Up</h2>
 			<label>
+				First and Last Name
 				<Field type="text" name="username" placeholder="Full name" />
+				{touched.username && errors.username && <p>{errors.username}</p>}
 			</label>
 			<label>
+				Email:
 				<Field type="email" name="email" placeholder="Email" />
+				{touched.email && errors.email && <p>{errors.email}</p>}
 			</label>
 			<label>
+				Password:
 				<Field type="password" name="password" placeholder="Password" />
+				{touched.password && errors.password && <p>{errors.password}</p>}
 			</label>
-			<Field component="select" name="role">
-				<option>Select your Role</option>
-				<option value="parent">Parent</option>
-				<option value="caregiver">Caregiver</option>
-			</Field>
+			<label>
+				Role:
+				<Field component="select" name="role">
+					<option>Select your Role</option>
+					<option value="parent">Parent</option>
+					<option value="caregiver">Caregiver</option>
+				</Field>
+				{touched.role && errors.role && <p>{errors.role}</p>}
+			</label>
+
 			<button type="submit">Sign Up</button>
 		</Form>
 	);
@@ -33,6 +45,20 @@ const FormikLoginForm = withFormik({
 			role: role || ''
 		};
 	},
+
+	//======VALIDATION SCHEMA==========
+	validationSchema: Yup.object().shape({
+		username: Yup.string().required('Full name is required.'),
+		email: Yup.string()
+			.email()
+			.required('Email address is required.'),
+		password: Yup.string()
+			.min(6)
+			.required('Password of at least 6 characters is required.'),
+		role: Yup.string()
+			.ensure()
+			.required('Role selection is required')
+	}),
 
 	handleSubmit(values) {
 		console.log(values);
